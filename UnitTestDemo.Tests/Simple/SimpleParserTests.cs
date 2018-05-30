d0a12c7701fdbc795b9b1dfa731e9878a191d68d
lorifpeterson <lori.f.peterson@gmail.com>
2018-05-30 11:28:18 -0500

    Modified Namespace

--- UnitTestDemo.Tests/Simple/SimpleParserTests.cs
+++ UnitTestDemo.Tests/Simple/SimpleParserTests.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Cision.UnitTestDemo.Simple
+namespace UnitTestDemo.Simple
 {
     [TestClass]
     public class SimpleParserTests
