be775d7aa56234ddb5052fd48f1e6415b19382f2
lorifpeterson <lori.f.peterson@gmail.com>
2018-05-30 12:35:29 -0500

    updated assembly info

--- UnitTestDemo/Properties/AssemblyInfo.cs
+++ UnitTestDemo/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Cision.UnitTestDemo")]
+[assembly: AssemblyTitle("UnitTestDemo")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Cision Inc.")]
-[assembly: AssemblyProduct("Cision.UnitTestDemo")]
-[assembly: AssemblyCopyright("Copyright © Cision Inc. 2012")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestDemo")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
