88836dbd2ccc13aaab1f472f4fca2e721ed0e3ae
lorifpeterson <lori.f.peterson@gmail.com>
2018-05-30 12:03:16 -0500

    modified namespace

--- UnitTestDemo.Web/Global.asax.cs
+++ UnitTestDemo.Web/Global.asax.cs
@@ -5,9 +5,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
-using Cision.UnitTestDemo.Web.IoC;
+using UnitTestDemo.Web.IoC;
 
-namespace Cision.UnitTestDemo.Web
+namespace UnitTestDemo.Web
 {
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
