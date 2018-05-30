4eb36fdb33e8d519d8f9d0c7e2d05738782e485a
lorifpeterson <lori.f.peterson@gmail.com>
2018-05-30 12:17:57 -0500

    modified namespace

--- UnitTestDemo/Calculator/IUSD_ExchangeRateFeed.cs
+++ UnitTestDemo/Calculator/IUSD_ExchangeRateFeed.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Cision.UnitTestDemo.Calculator
+namespace UnitTestDemo.Calculator
 {
     public interface IUSD_ExchangeRateFeed
     {
